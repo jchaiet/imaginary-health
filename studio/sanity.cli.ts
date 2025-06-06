1936fb383ef077ace5eddc51868e65dca812493d
Joe Chaiet <jchaiet@hotmail.com>
2025-06-06 09:32:59 -0400

    Self host studio

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,6 +5,9 @@ export default defineCliConfig({
     projectId: "m4mnm2dh",
     dataset: "production",
   },
+  project: {
+    basePath: "/studio",
+  },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
