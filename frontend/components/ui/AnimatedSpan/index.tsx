795a1bf5a506e8d9be89ea54bc2e0fcf05dfca58
Joe Chaiet <jchaiet@hotmail.com>
2025-06-22 16:29:46 -0400

    Update tab component

--- frontend/components/ui/AnimatedSpan/index.tsx
+++ frontend/components/ui/AnimatedSpan/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 export function AnimatedSpan({
