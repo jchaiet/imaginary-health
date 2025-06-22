68e7617eb67e7ed59b705c059c4561f87970efd9
Joe Chaiet <jchaiet@hotmail.com>
2025-06-22 16:40:52 -0400

    Update tab component

--- frontend/components/ui/AnimatedSpan/index.tsx
+++ frontend/components/ui/AnimatedSpan/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "./styles.module.css";
 
 export function AnimatedSpan({
@@ -10,19 +10,67 @@ export function AnimatedSpan({
   prevText?: string;
   className?: string;
 }) {
+  const [displayedPrevText, setDisplayedPrevText] = useState(prevText);
+  const [currentTextToDisplay, setCurrentTextToDisplay] = useState(text);
+
   const [prevAnimClass, setPrevAnimClass] = useState<string | null>(null);
   const [currentAnimClass, setCurrentAnimClass] = useState<string | null>(null);
 
+  const timeoutId = useRef<NodeJS.Timeout | null>(null);
+
   useEffect(() => {
-    //Slide left
-    setPrevAnimClass(styles.slideOff);
-    setCurrentAnimClass(styles.slideOn);
-  }, [text]);
+    if (timeoutId.current) {
+      clearTimeout(timeoutId.current);
+    }
+
+    setCurrentTextToDisplay(text);
+    setCurrentAnimClass(styles.reset);
+
+    setDisplayedPrevText(prevText);
+    setPrevAnimClass(null);
+
+    requestAnimationFrame(() => {
+      void document.body.offsetHeight;
+
+      setPrevAnimClass(styles.slideOff);
+      setCurrentAnimClass(styles.slideOn);
+    });
+
+    timeoutId.current = setTimeout(() => {
+      setDisplayedPrevText(undefined);
+      setPrevAnimClass(null);
+    }, 300);
+
+    return () => {
+      if (timeoutId.current) {
+        clearTimeout(timeoutId.current);
+      }
+    };
+  }, [text, prevText]);
+
+  useEffect(() => {
+    if (!displayedPrevText && !currentTextToDisplay && text) {
+      setCurrentTextToDisplay(text);
+      setCurrentAnimClass(null);
+    }
+  });
+
+  // useEffect(() => {
+  //   //Slide left
+  //   setPrevAnimClass(styles.slideOff);
+  //   setCurrentAnimClass(styles.slideOn);
+  // }, [text]);
 
   return (
     <span className={`${styles.animatedSpan} ${className ?? ""}`}>
-      <span className={`${styles.previous} ${prevAnimClass}`}>{prevText}</span>
-      <span className={`${styles.current} ${currentAnimClass}`}>{text}</span>
+      {displayedPrevText !== undefined && (
+        <span className={`${styles.previous} ${prevAnimClass}`}>
+          {displayedPrevText}
+        </span>
+      )}
+      <span className={`${styles.current} ${currentAnimClass}`}>
+        {currentTextToDisplay}
+      </span>
     </span>
   );
 }
